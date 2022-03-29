14d12ea5a8baa1f8fe0dfe8c5319915a88f803e3
sylvain <sylvain.vialatte@gmail.com>
2022-03-29 22:26:05 +0200

    blanc

--- src/TimeTracker.Apps/ViewModels/ProjetViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/ProjetViewModel.cs
@@ -16,52 +16,6 @@ namespace TimeTracker.Apps.ViewModels
 {
     internal class ProjetViewModel : ViewModelBase
     {
-        private string _text;
-        private string _description;
-
-        public string Text
-        {
-            get => _text;
-            set => SetProperty(ref _text, value);
-        }
-
-        public string Description
-        {
-            get => _description;
-            set => SetProperty(ref _description, value);
-        }
-
-        public ICommand NouveauProjet { get; }
-
-        public ICommand CancelCommand { get; }
-
-        public ProjetViewModel()
-        {
-            NouveauProjet = new Command(NewProjet);
-            CancelCommand = new Command(Cancel);
-        } 
-
-        private void NewProjet()
-        {
-            string text = Text;
-            if(string.IsNullOrEmpty(text))
-            {
-                return;
-            }
-
-            var projetService = DependencyService.Get<ProjetService>();
-            //projetService.AddProject(Text, Description);
-            //projetService.addProjet(Text, Description);
-
-            new Projet(0, _text, _description, 0);
-            INavigationService navigationService = DependencyService.Get<INavigationService>();
-            navigationService.PushAsync<AccueilView>();
-        }
-
-        private void Cancel()
-        {
-            INavigationService navigationService = DependencyService.Get<INavigationService>();
-            navigationService.PushAsync<AccueilView>();
-        }
+       
     }
 }
