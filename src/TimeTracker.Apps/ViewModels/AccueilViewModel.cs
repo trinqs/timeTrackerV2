9d1a8c5a77404901bfca2e59296c08bfbfe443fd
Margot <margot@DESKTOP-U6FGHB3.localdomain>
2022-03-29 04:09:35 +0200

    update Profil,Projet,Accueil(View,Model) et test accueil

--- src/TimeTracker.Apps/ViewModels/AccueilViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/AccueilViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using TimeTracker.Apps.Modele;
 using TimeTracker.Apps.Pages;
@@ -42,7 +43,6 @@ namespace TimeTracker.Apps.ViewModels
             INavigationService navigationService = DependencyService.Get<INavigationService>();
             navigationService.PushAsync<ProjetView>();
         }
-
 
         private void creerJeuTest()
         {
