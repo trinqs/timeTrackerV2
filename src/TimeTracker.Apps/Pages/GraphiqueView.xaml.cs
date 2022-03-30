742bc7dbfe891a02dee2ad55aa6c656a60234e4e
sylvain <sylvain.vialatte@gmail.com>
2022-03-30 19:47:39 +0200

    graphique

--- src/TimeTracker.Apps/Pages/GraphiqueView.xaml.cs
+++ src/TimeTracker.Apps/Pages/GraphiqueView.xaml.cs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿using Microcharts;
+
+using Microcharts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,29 +16,6 @@ namespace TimeTracker.Apps.Pages
     {
         public GraphiqueView(List<ChartEntry> entries)
         {
-            InitializeComponent();
-            BindingContext = new GraphiqueViewModel(entries);
-        }
-    }
-=======
-﻿using Microcharts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TimeTracker.Apps.ViewModels;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace TimeTracker.Apps.Pages
-{
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class GraphiqueView : ContentPage
-    {
-        public GraphiqueView()
-        {
-            List<ChartEntry> entries=null;
             InitializeComponent();
             BindingContext = new GraphiqueViewModel(entries);
         }
