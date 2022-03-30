d7be95036fabe739553c6274e12696d7ec3316e8
sylvain <sylvain.vialatte@gmail.com>
2022-03-30 20:08:40 +0200

    graphique

--- src/TimeTracker.Apps/ViewModels/GraphiqueViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/GraphiqueViewModel.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using Microcharts;
+using Microcharts;
 using SkiaSharp;
 using Storm.Mvvm;
 using System;
@@ -87,35 +86,3 @@ namespace TimeTracker.Apps.ViewModels
         }
     }
 }
-=======
-﻿using Microcharts;
-using Storm.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
-
-namespace TimeTracker.Apps.ViewModels
-{
-    internal class GraphiqueViewModel : ViewModelBase
-    {
-
-        private PieChart _pieChart;
-        public PieChart PieChart
-        {
-            get => _pieChart;
-            set => SetProperty(ref _pieChart, value);
-        }
-
-        public GraphiqueViewModel(List<ChartEntry> entries)
-        {
-            List<ChartEntry> entriesTest = new List<ChartEntry>();
-            entriesTest.Add(new ChartEntry(212) { Label = "UWP", ValueLabel = "212"});
-            entriesTest.Add(new ChartEntry(248) { Label = "Android", ValueLabel = "248" });
-            entriesTest.Add(new ChartEntry(128) { Label = "iOS", ValueLabel = "128"  });
-            entriesTest.Add(new ChartEntry(514) { Label = "Shared", ValueLabel = "514" });
-            PieChart = new PieChart() { Entries = entriesTest };
-        }
-    }
-}
->>>>>>> 1a9b145329a041e8af9594502a2fa431d7782cd8
