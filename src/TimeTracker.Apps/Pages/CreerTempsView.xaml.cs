5e2a572c168c410b7c0bf8abff2d8f7bae554b5d
sylvain <sylvain.vialatte@etu.univ-orleans.fr>
2022-04-01 14:34:02 +0200

    creerTemps

---
+++ src/TimeTracker.Apps/Pages/CreerTempsView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeTracker.Apps.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreerTempsView : ContentPage
+    {
+        public CreerTempsView()
+        {
+            InitializeComponent();
+        }
+
+        async private void ProjetSelected(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            await Navigation.PushAsync(new ProjetView((Dtos.Projects.ProjectItem)e.Item));
+        }
+    }
+}
---
+++ src/TimeTracker.Apps/ViewModels/CreerTempsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Storm.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TimeTracker.Apps.WebService;
+using TimeTracker.Dtos.Projects;
+
+namespace TimeTracker.Apps.ViewModels
+{
+    internal class CreerTempsViewModel : ViewModelBase
+    {
+
+        private ObservableCollection<ProjectItem> _projets;
+        public ObservableCollection<ProjectItem> Projets
+        {
+            get => _projets;
+            set => SetProperty(ref _projets, value);
+        }
+
+
+        private ObservableCollection<TaskItem> _taches;
+        public ObservableCollection<TaskItem> Taches
+        {
+            get => _taches;
+            set => SetProperty(ref _taches, value);
+        }
+
+        public CreerTempsViewModel()
+        {
+            Projets = new ObservableCollection<ProjectItem>();
+            Taches = new ObservableCollection<TaskItem>();
+            getProjets();
+        }
+
+        private async void getProjets()
+        {
+            Projets = await ProjetService.GetAllProject();
+            OnPropertyChanged(nameof(Projets));
+        }
+
+    }
+}
