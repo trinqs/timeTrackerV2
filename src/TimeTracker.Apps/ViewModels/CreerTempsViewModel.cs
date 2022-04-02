4c4fd718a476dc1e0825d0974ff0ca759e7439c0
sylvain <sylvain.vialatte@etu.univ-orleans.fr>
2022-04-02 13:49:13 +0200

    creerTemps

--- src/TimeTracker.Apps/ViewModels/CreerTempsViewModel.cs
+++ src/TimeTracker.Apps/ViewModels/CreerTempsViewModel.cs
@@ -3,8 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 using TimeTracker.Apps.WebService;
 using TimeTracker.Dtos.Projects;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Storm.Mvvm.Services;
 
 namespace TimeTracker.Apps.ViewModels
 {
@@ -26,14 +30,50 @@ namespace TimeTracker.Apps.ViewModels
             set => SetProperty(ref _taches, value);
         }
 
+        private ProjectItem _project;
+        public ProjectItem Project
+        {
+            get => _project;
+            set => SetProperty(ref _project, value);
+        }
+
+        private bool _projetChoisiBool=false;
+        public bool ProjetChoisiBool
+        {
+            get => _projetChoisiBool;
+            set => SetProperty(ref _projetChoisiBool, value);
+        }
+
+        public ICommand ProjetSelectionneCommand { get; }
+        public ICommand TachesSelectionneCommand { get; }
+
         public CreerTempsViewModel()
         {
             Projets = new ObservableCollection<ProjectItem>();
             Taches = new ObservableCollection<TaskItem>();
-            getProjets();
+            ProjetSelectionneCommand = new Command<ProjectItem>(ProjetSelectionne);
+            TachesSelectionneCommand = new Command<TaskItem>(TacheSelectionne);
+            GetProjets();
         }
 
-        private async void getProjets()
+        private async void ProjetSelectionne(ProjectItem projectItem)
+        {   
+            Taches = await TaskService.GetAllTask((int)projectItem.Id);
+            Project = projectItem;
+            ProjetChoisiBool = true;
+        }
+
+        private async void TacheSelectionne(TaskItem taskItem)
+        {
+            await TimeService.AddTime( (int)Project.Id, (int) taskItem.Id, Preferences.Get("depart",null), Preferences.Get("fin",null) );
+            Preferences.Remove("depart");
+            Preferences.Remove("fin");
+            Preferences.Set("timerEnCours", false);
+            INavigationService navigationService = DependencyService.Get<INavigationService>();
+            await navigationService.PopAsync();
+        }
+
+        private async void GetProjets()
         {
             Projets = await ProjetService.GetAllProject();
             OnPropertyChanged(nameof(Projets));
